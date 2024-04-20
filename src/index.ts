a3390f9e6ac60e655c6ad09e09af30d9167b6612
Tushar Rawat <tusharrawat52@gmail.com>
2024-04-20 16:38:26 +0530

    initilized

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import WebSocket, {WebSocketServer} from "ws";
+import http from 'http'
+
+const server = http.createServer(function(request : any , response : any){
+    console.log(new Date() + 'Recieved request for ' + request.url);
+    response.end("hi there");
+})
+
+const wss = new WebSocketServer({server});
+let userCount = 0; 
+
+wss.on('connection' , function(socket) {
+    socket.on('error' , (err) => {
+        console.error(err);
+    })
+
+    socket.on('message' , function message(data , isBinary) {
+        wss.clients.forEach(function each(client){
+            if(client.readyState === WebSocket.OPEN){
+                client.send(data , {binary : isBinary});
+            }
+        });
+    });
+    console.log("user connected" , ++userCount);
+    socket.send("Hello ! message from server!!");
+})
+
+
+
+
+server.listen(8080 , function() {
+    console.log(new Date() + "server is listening on port : 8080");
+})
